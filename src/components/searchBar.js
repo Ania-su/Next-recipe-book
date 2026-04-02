20a6b1cc89ca6592cbd273ef9125846c1fd1e6cc
Ania-su <antsaaa.randria@gmail.com>
2026-04-02 12:09:56 +0300

    feat: add filtered search bar

---
+++ src/components/searchBar.js
@@ -0,0 +1,21 @@
+import { Search } from "@deemlol/next-icons";
+import styles from "../app/page.module.css";
+
+export default function SearchBar({ searchQuery, setSearchQuery }) {
+  const handleInputChange = (e) => {
+    setSearchQuery(e.target.value.toLowerCase());
+  };
+
+  return (
+    <div className={styles.items}>
+      <Search className={styles.searchIcon}/>
+      <input
+        type="text"
+        placeholder="Search recipe by name..."
+        value={searchQuery}
+        onChange={handleInputChange}
+        className={styles.input}
+      />
+    </div>
+  );
+}
